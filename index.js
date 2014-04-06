b51842ded999a90bd5743d57616368b6fd3049a3
Brian Johnson <bjohnson@trulia.com>
2014-04-05 21:23:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+    http = require('http'),
+    https = require('https'),
+    path = require('path'),
+    fs = require('fs'),
+    mongoose = require('mongoose');
+/**
+ * Mongoose
+ */
+mongoose.connect('mongodb://localhost/project_one');
+/**
+ * Express
+ */
+var app = express();
+/**
+ * Create the server
+ */
+var server = http.createServer(app);
+/**
+ *
+ */
+app.use('*', app.router);
+/**
+ * Static resources
+ */
+app.use(function (req, res, next) {
+    'use strict';
+    res.setHeader('Expires', 'Fri, 30 Oct 2014 14:19:41 GMT');
+    res.setHeader('Last-Modified', 'Fri, 30 Oct 1998 14:19:41 GMT');
+    next();
+});
+var coreAssets = __dirname + '/app';
+app.use(express.static(coreAssets));
+/**
+ * Start Server
+ */
+server.listen(80, function () {
+    'use strict';
+    console.log('Listening on port ' + 80);
+});
