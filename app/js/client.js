19e3bda83d84b205d75d273b676a10b8bf4c4e4c
Laura Klemme <laura.klemme@gmail.com>
2014-04-06 18:36:53 -0700

    exercises added json db added check price endpoint

--- app/js/client.js
+++ app/js/client.js
@@ -1,19 +1,26 @@
 /**
  * My Client Application
  */
-angular.module('client', ['stripe'])
+angular.module('client', [])
     .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
         'use strict';
-        $scope.mySillyText = "Welcome to Brian's Awesome Products";
-        $scope.saveCustomer = function stripeResponseHandler(status, response) {
-            if (response.error) {
-                // show the errors on the form
-                $(".payment-errors").text(response.error.message);
-            } else {
-                var token = response.id;
-                $http.post('/stripe', {stripeToken: token});
-            }
+        $scope.mySillyText = "Welcome to Laura's Awesome Products";
+        $scope.priceMessage = "";
+        $scope.bikeColor = "";
+
+        $scope.popAlert = function () {
+            alert('hello');
         };
-    }]).run([function () {
-        Stripe.setPublishableKey('pk_test_Cg7dxMFhOjXhdtows9LKrvbN');
+        $scope.checkPrice = function (bikeColor) {
+            $scope.bikeColor = bikeColor;
+            $http.get('/checkPrice', {params: {bikeColor: bikeColor}})
+                .success(function (response) {
+                    $scope.priceMessage = response.availability;
+                });
+
+        };
+
+    }]).
+    run([function () {
+
     }]);
--- index.js
+++ index.js
@@ -5,12 +5,9 @@ var express = require('express'),
     http = require('http'),
     https = require('https'),
     path = require('path'),
-    fs = require('fs'),
-    mongoose = require('mongoose');
-/**
- * Mongoose
- */
-mongoose.connect('mongodb://localhost/project_one');
+    fs = require('fs');
+
+
 /**
  * Express
  */
@@ -26,31 +23,8 @@ app.use(app.router);
  * Stripe endpoint
  */
 /**
- * Get Stripe Library
- */
-var Stripe = require('stripe');
-var stripe = Stripe("sk_test_VF8whfxJ7JdWNz4PCR2kdn0b");
-app.use(express.bodyParser());
-app.post('/stripe', function (req, res) {
-    'use strict';
-    console.log(req.is('json'));
-    var stripeToken = req.body.stripeToken;
-    var charge = stripe.charges.create({
-        amount: 1000, // amount in cents, again
-        currency: "usd",
-        card: stripeToken,
-        description: "payinguser@example.com"
-    }, function (err, charge) {
-        if (err && err.type === 'StripeCardError') {
-            // The card has been declined
-            console.log("Card declined");
-        }
-        //Charged the card
-        console.log(charge);
-    });
-});
 
-/**
+ /**
  * Static resources
  */
 app.use(function (req, res, next) {
@@ -61,8 +35,23 @@ app.use(function (req, res, next) {
 });
 var coreAssets = __dirname + '/app';
 app.use(express.static(coreAssets));
+
 /**
- * Start Server
+ * Check price Endpoint
+ */
+app.get('/checkPrice', function (request, response) {
+    var bikeColor = request.query.bikeColor;
+
+    var prices = require('./prices');
+    response.json({
+        availability: prices[bikeColor]
+    });
+
+
+});
+/**
+ Start
+ Server
  */
 app.listen(8080, function () {
     'use strict';
